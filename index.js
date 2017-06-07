60d2e9e25d157c14b3d79768dd9b999121373fb0
xuld <xuld@vip.qq.com>
2017-06-07 22:15:23 +0800

    改进源提示

--- index.js
+++ index.js
@@ -9,29 +9,40 @@ module.exports = function AutoPrefixer(file, options, done) {
         to: file.srcPath,
         map: file.sourceMap && {
             prev: file.sourceMapObject,
-            from: file.srcPath,
-            annotation: false
+            annotation: false,
+            inline: false
         }
     }, options);
+    if (options.map && options.map.prev) {
+        for (var i = 0; i < options.map.prev.sources.length; i++) {
+            options.map.prev.sources[i] = file.relative(options.map.prev.sources[i]);
+        }
+    }
 
     // 生成。
     postcss([autoPrefixer(options)]).process(file.content, options).then(function (result) {
-        var warnings = result.warnings();
-        if (warnings.length) {
-            for (var i = 0; i < warnings.length; i++) {
-                file.warning({
-                    plugin: AutoPrefixer.name,
-                    message: warnings[i].text || warnings[i].reason || warnings[i].message,
-                    line: warnings[i].line - 1,
-                    column: warnings[i].column - 1,
-                });
+        if (file.errorCount === 0) {
+            var warnings = result.warnings();
+            if (warnings.length) {
+                for (var i = 0; i < warnings.length; i++) {
+                    file.warning({
+                        plugin: AutoPrefixer.name,
+                        message: warnings[i].text || warnings[i].reason || warnings[i].message,
+                        line: warnings[i].line - 1,
+                        column: warnings[i].column - 1,
+                    });
+                }
             }
         }
 
         // 保存。
         file.content = result.css;
         if (result.map) {
-            file.sourceMapObject = result.map;
+            const map = result.map.toJSON();
+            for (var i = 0; i < map.sources.length; i++) {
+                map.sources[i] = file.resolve(map.sources[i]);
+            }
+            file.sourceMapObject = map;
         }
     }, function (error) {
         file.error({
